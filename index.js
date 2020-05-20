3f7a8be8fa4e5b2054008193fde1acaa8869b7bc
AhmedAAlraddadi <64801803+AhmedAAlraddadi@users.noreply.github.com>
2020-05-21 03:11:23 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function (registration) {
+    console.log(
+        'ServiceWorker registration successful with scope: ',
+        registration.scope
+    );
+  });
+}
+
+
 $(document).ready(function(){
   $("img").click(function(){
     $(this).hide();
   });
 });
+
